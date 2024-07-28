df140c64811902a9f0f8adae717febda4b813339
Aliasgar <jhalodwalaaliasgar53@gmail.com>
2024-07-28 18:02:57 +0530

    optimize - removed useless getme route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,15 +1,8 @@
 import express from "express";
-import {
-	getMe,
-	login,
-	logout,
-	signup,
-} from "../controllers/auth.controller.js";
-import protectRoute from "../middlewares/protectRoute.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
