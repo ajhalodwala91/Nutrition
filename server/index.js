75ae8502ab34c08fd3b222d9d3bc775df7fbc534
Aliasgar <jhalodwalaaliasgar53@gmail.com>
2024-08-19 20:32:27 +0530

    changes for production -import cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectToDb from "./configs/mongoose.config.js";
 import authRoutes from "./routes/auth.routes.js";
 
